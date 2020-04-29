6a5f5c94c1b37e93c9a58468cb643c6db321b3b1
Srinadh Biroji <bairojisrinadh@gmail.com>
2020-04-29 18:00:42 +0530

    minor styling fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,10 @@
 /** Hamburger Menu onClick Event */
 const menuBtn = document.querySelector(".menu-btn");
 const nav_sm = document.querySelector(".nav-sm");
+const nav_sm_a = document.querySelectorAll(".nav-sm a[href^='#']");
 let menuOpen = false;
 
-menuBtn.addEventListener("click", () => {
+function toggle() {
   if (!menuOpen) {
     menuBtn.classList.add("open");
     nav_sm.classList.add("open");
@@ -13,4 +14,18 @@ menuBtn.addEventListener("click", () => {
     nav_sm.classList.remove("open");
     menuOpen = false;
   }
+}
+
+menuBtn.addEventListener("click", toggle);
+
+/** click eventListener for all navigation a tags */
+nav_sm_a.forEach(anchor => {
+  anchor.addEventListener("click", e => {
+    e.preventDefault();
+    toggle();
+    let id = ("" + anchor.hash).substr(1);
+    document.getElementById(id).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
 });
