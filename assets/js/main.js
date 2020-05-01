99563fcbf245be2bd375926241b0800a72cf766e
Srinadh Biroji <bairojisrinadh@gmail.com>
2020-05-01 16:24:49 +0530

    css & redirection fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,11 +18,29 @@ function toggle() {
 
 menuBtn.addEventListener("click", toggle);
 
-/** click eventListener for all navigation a tags */
+/** click eventListener for all nav-sm a tags */
 nav_sm_a.forEach(anchor => {
   anchor.addEventListener("click", e => {
     e.preventDefault();
     toggle();
+    let id = ("" + anchor.hash).substr(1);
+    document.getElementById(id).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
+
+
+const nav_lg_a = document.querySelectorAll(".nav-lg a[href^='#']");
+
+/** click eventListener for all nav-lg a tags */
+nav_lg_a.forEach(anchor => {
+  anchor.addEventListener("click", e => {
+    e.preventDefault();
+    //removing active class on all anchor tags.
+    nav_lg_a.forEach(a => a.classList.remove("active"));
+    //adding active class to specific anchor tag.
+    anchor.classList.add("active");
     let id = ("" + anchor.hash).substr(1);
     document.getElementById(id).scrollIntoView({
       behavior: "smooth"
